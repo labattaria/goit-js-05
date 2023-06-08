4fa666bf169a9b8acf526e96f414ec443d5fcac1
labattaria <hallucinate322@gmail.com>
2023-06-08 03:06:05 +0300

    successfully done

---
+++ js/03.js
@@ -0,0 +1,33 @@
+class Storage {
+    constructor(items) {
+        this.items = [...items];
+    }
+
+    getItems() {
+        return this.items;
+    }
+
+    addItem(item) {
+        this.items.push(item);
+    }
+
+    removeItem(itemToRemove) {
+        return this.items = this.items.filter(item => item !== itemToRemove);
+    }
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
